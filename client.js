6d29520c01b1941e22816f5999a995564ab6ebc9
Jessica Stephens <jeslstep1@gmail.com>
2018-10-07 18:59:53 -0500

    stretch mode

--- client.js
+++ client.js
@@ -60,41 +60,55 @@ function appendEmployeeList() {
     }
 }
 
-// removing employees 
+// removing employees and salary value from the arrays and dom
 function removeEmployeesButton(){
-    console.log('removing');
+    console.log('removing employee');
     let selectedItem = $(this).parent().text();
-    console.log(selectedItem);
+    console.log('targeting selectedItem',selectedItem);
     for(let i = 0; i < employeesArray.length; i++) {
-        if(selectedItem.includes(employeesArray[i].firstName)){
-            console.log('deleted');
+        if(selectedItem.includes(employeesArray[i].idNumber)){
+            console.log('deleted',employeesArray[i].idNumber );
+            console.log('salary ',employeesArray[i].annualSalary );
+            let salary = parseInt(employeesArray[i].annualSalary/12);
             employeesArray.splice(i, 1);
             $(this).parent().remove();
-            return true;
-        }   
+            console.log(employeesArray);
+// remove monthly cost if employee is deleted
+    console.log('targeting selectedItem',salary);
+    for(let i = 0; i < totMonthCostArray.length; i++) {
+        console.log('totMonthCostArray[i]',totMonthCostArray[i]);
+     if(totMonthCostArray[i] == salary){
+         console.log('deleted',totMonthCostArray[i] );
+         totMonthCostArray.splice(i, 1);
+         console.log(totMonthCostArray);
+         calcuTotalMonthlyCost ();
+     }
     }
 }
-
-// indivdual monthly cost array
-let indivdualCostArray=[];
-
+}
+}
+// array to store monthly costs per employee
+let totMonthCostArray =[];
 // calculates Total monthly cost of each employee added 
 function calcuTotalMonthlyCost (){
     console.log('calculating');
     let yearSal = $('#annualSalary').val();
-    let individualmonthlyCost= yearSal/12
-    indivdualCostArray.push(individualmonthlyCost);
-    console.log('this employee costs',individualmonthlyCost,'each month');
-    console.log('monthly cost of employe is', indivdualCostArray);
+    console.log('this employess yearly salary is', yearSal);
+    let individualMonthlyCost= yearSal/12;
+    totMonthCostArray.push(individualMonthlyCost);
+    console.log('this employee costs',individualMonthlyCost,'each month');
 // caluculates the total cost of all the employess added
-    let monthlyCost =0;
-    for (let monthCost of indivdualCostArray){
-        console.log('in the for');
-        monthlyCost = monthlyCost+monthCost;
-        console.log('monthlyCost', monthlyCost);
+    let totalMonthlyCost = 0;
+    for (let inMonthCost of totMonthCostArray)
+        totalMonthlyCost += inMonthCost;
+        console.log('totalMonthlyCost', totalMonthlyCost);
 // appending total monthly cost to dom
         let element= $('#totalMonlthyCost');
         element.empty();
-        element.append('<h3>'+ monthlyCost +'</h3>'); 
-        }
-    }
+        element.append('<h3>'+ totalMonthlyCost +'</h3>'); 
+}
+
+
+
+
+    
